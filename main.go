896d36ec96c1a08ee2893cedf0607f873da654a6
willings <willings.zhang@gmail.com>
2016-02-19 00:50:18 +0900

    Add cache support

--- main.go
+++ main.go
@@ -5,41 +5,67 @@ import (
 	"fmt"
 	"github.com/willings/proxypool/provider"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/urlfetch"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const AUTH_ON = false
 
-const API_KEY = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+const API_KEY = "YOUR_SEC_KEY"
 
+const (
+	QYERY_APIKEY    = "apikey"
+	QUERY_PROVIDERS = "providers"
+)
 
 func init() {
 	http.HandleFunc("/", handle)
-	http.HandleFunc("/proxylist/refresh.json", handle_refresh_json)
+	http.HandleFunc("/proxy.json", handle_cached_json)
+	http.HandleFunc("/refresh", handle_refresh_json)
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1><It Works!/h1>")
 }
 
-func handle_refresh_json(w http.ResponseWriter, r *http.Request) {
-	if AUTH_ON {
-		apikey := r.URL.Query().Get("apikey") // TODO
-		if apikey != API_KEY {
-			errJson(w, ACCESS_DENIED)
-			return
-		}
+func handle_cached_json(w http.ResponseWriter, r *http.Request) {
+	if !checkPermission(w, r) {
+		return
+	}
+	providers := r.URL.Query().Get(QUERY_PROVIDERS)
+
+	ctx := appengine.NewContext(r)
+	key := datastore.NewKey(ctx, QUERY_PROVIDERS, providers, 0, nil)
+
+	cache := &ProxyList{}
+	err := datastore.Get(ctx, key, cache)
+
+	if err != nil || time.Now().Sub(cache.Timestamp) > time.Hour {
+		handle_refresh_json(w, r)
+		return
 	}
 
-	providers := r.URL.Query().Get("providers")
+	ret, _ := json.Marshal(cache.Proxies)
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, string(ret))
+}
+
+func handle_refresh_json(w http.ResponseWriter, r *http.Request) {
+	if !checkPermission(w, r) {
+		return
+	}
+
+	providers := r.URL.Query().Get(QUERY_PROVIDERS)
 	providerArr := strings.Split(providers, "|")
 
 	loaders := make([]provider.ProxyProvider, 0)
 
 	for _, name := range providerArr {
-		if name == "any" {
+		if name == "all" || name == "" {
 			loaders = provider.CreateAllProvider()
 			break
 		} else {
@@ -48,7 +74,6 @@ func handle_refresh_json(w http.ResponseWriter, r *http.Request) {
 				loaders = append(loaders, loader)
 			}
 		}
-
 	}
 
 	if len(loaders) == 0 {
@@ -63,8 +88,35 @@ func handle_refresh_json(w http.ResponseWriter, r *http.Request) {
 	p.SetClient(client)
 	items, _ := p.Load()
 
+	cache := &ProxyList{
+		Timestamp: time.Now(),
+		Proxies:   make([]provider.ProxyItem, len(items)),
+	}
+	for i, item := range items {
+		cache.Proxies[i] = *item
+	}
+
+	key := datastore.NewKey(ctx, QUERY_PROVIDERS, providers, 0, nil)
+	datastore.Put(ctx, key, cache)
+
 	ret, _ := json.Marshal(items)
 
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, string(ret))
+}
+
+func checkPermission(w http.ResponseWriter, r *http.Request) bool {
+	if AUTH_ON {
+		apikey := r.URL.Query().Get(QYERY_APIKEY) // TODO
+		if apikey != API_KEY {
+			errJson(w, ACCESS_DENIED)
+			return false
+		}
+	}
+	return true
+}
+
+type ProxyList struct {
+	Timestamp time.Time
+	Proxies   []provider.ProxyItem
 }
